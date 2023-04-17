a020883474fe7f289dce5bc1f8e21cdb322b81ee
JulieBenchuk <juliebenchuk@gmail.com>
2023-04-17 23:08:29 +0300

    refactor: created theme of app

--- src/index.tsx
+++ src/index.tsx
@@ -6,13 +6,27 @@ import * as serviceWorker from './serviceWorker';
 import App from "./app/App";
 import {Provider} from "react-redux";
 import {store} from "./app/store";
-import {BrowserRouter, HashRouter} from "react-router-dom";
+import {HashRouter} from "react-router-dom";
+import {createTheme, ThemeProvider} from "@mui/material";
+
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: "#5f866f"
+        },
+        secondary: {
+            main: "#A970EB"
+        }
+    }
+})
 
 ReactDOM.render(
     <HashRouter>
-        <Provider store={store}>
-            <App/>
-        </Provider>
+        <ThemeProvider theme={theme}>
+            <Provider store={store}>
+                <App/>
+            </Provider>
+        </ThemeProvider>
     </HashRouter>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
